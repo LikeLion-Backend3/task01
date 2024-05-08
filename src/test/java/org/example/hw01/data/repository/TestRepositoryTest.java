7f1e96564e9650f8afd54061470911190b1e75b9
HwangHyeongjin <80672495+HwangHyeongjin@users.noreply.github.com>
2024-05-08 14:51:32 +0900

    Update TestRepositoryTest.java

--- src/test/java/org/example/hw01/data/repository/TestRepositoryTest.java
+++ src/test/java/org/example/hw01/data/repository/TestRepositoryTest.java
@@ -9,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.List;
 
 @SpringBootTest
-class TestRepositoryTest {
+class TaskRepositoryTest {
 
     @Autowired
     BabyLionRepository babyLionRepository;
@@ -23,30 +23,34 @@ class TestRepositoryTest {
         // 테스트 데이터 생성
         BabyLion babyLion = new BabyLion();
         babyLion.setName("황형진");
+        babyLion.setMajor("소프트웨어전공");
         babyLion.setEmail("gudwls10@kangnam.ac.kr");
-        babyLion.setNumber("010-3036-9968");
+        babyLion.setPhonenum("010-3036-9968");
         babyLion.setPassword("2580");
         babyLion.setProfile("pic0");
 
         BabyLion babyLion1 = new BabyLion();
         babyLion1.setName("황형진1");
+        babyLion1.setMajor("데이터사이언스전공");
         babyLion1.setEmail("gudwlsdlsms1@naver.com");
-        babyLion1.setNumber("010-6363-7912");
+        babyLion1.setPhonenum("010-6363-7912");
         babyLion1.setPassword("1111");
         babyLion1.setProfile("pic1");
 
 
         BabyLion babyLion2 = new BabyLion();
         babyLion2.setName("황형진2");
+        babyLion2.setMajor("산업경영전공");
         babyLion2.setEmail("ghkdgduwls10@nave.com");
-        babyLion2.setNumber("010-1234-1234");
+        babyLion2.setPhonenum("010-1234-1234");
         babyLion2.setPassword("1234");
         babyLion2.setProfile("pic2");
 
         BabyLion babyLion3 = new BabyLion();
         babyLion3.setName("황형진3");
+        babyLion3.setMajor("경영학전공");
         babyLion3.setEmail("botherhwang12@gmail.com");
-        babyLion3.setNumber("010-9999-9999");
+        babyLion3.setPhonenum("010-9999-9999");
         babyLion3.setPassword("9865");
         babyLion3.setProfile("pic3");
 
